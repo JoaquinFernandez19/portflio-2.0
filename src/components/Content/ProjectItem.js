14e0cc2a3ca463bd90ea2df52d8efd6a61cc724a
Joaquin Fernandez <copako12@gmail.com>
2020-04-25 19:09:15 -0300

    implementing projects section

--- src/components/Content/ProjectItem.js
+++ src/components/Content/ProjectItem.js
@@ -1,10 +1,26 @@
 import React from 'react';
-
+import { FaRegWindowClose } from 'react-icons/fa';
 const ProjectItem = (props) => {
+	const handleClick = (e) => {
+		let tar = e.target;
+		if (tar.classList.contains('img-project')) {
+			tar.nextSibling.style.display = 'flex';
+		}
+	};
+	const closeModal = (e) => {
+		if (e.target.tagName === 'path') {
+			e.target.parentNode.parentNode.style.display = 'none';
+		}
+		if (e.target.tagName === 'svg') {
+			e.target.parentNode.style.display = 'none';
+		}
+	};
+
 	return (
-		<div className="project-card">
-			<img src={props.img} alt="placeholder" />
+		<div className="project-card" onClick={handleClick}>
+			<img src={props.img} alt="placeholder" className="img-project" />
 			<div className="hidden animated fadeIn ">
+				<FaRegWindowClose className="pr-icon" onClick={closeModal} />
 				<p>{props.description}</p>
 				<div>
 					<a href={props.github} target="blank">
