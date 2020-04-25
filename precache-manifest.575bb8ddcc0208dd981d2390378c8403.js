d08593c6672f5ea6edc449afe90314949ffd3e31
Joaquin Fernandez <copako12@gmail.com>
2020-04-25 00:46:31 -0300

    Updates

---
+++ precache-manifest.575bb8ddcc0208dd981d2390378c8403.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd85a8d734c1cc4cb129b15f613cb1c6",
+    "url": "/portflio-2.0/index.html"
+  },
+  {
+    "revision": "467836afe80accfb15c7",
+    "url": "/portflio-2.0/static/css/main.56273b8e.chunk.css"
+  },
+  {
+    "revision": "ec4b77438d364c0ac35a",
+    "url": "/portflio-2.0/static/js/2.69185929.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portflio-2.0/static/js/2.69185929.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "467836afe80accfb15c7",
+    "url": "/portflio-2.0/static/js/main.9f928298.chunk.js"
+  },
+  {
+    "revision": "c0b11f37a969b2e8e4c5",
+    "url": "/portflio-2.0/static/js/runtime-main.284df615.js"
+  },
+  {
+    "revision": "e187a41a61532acbfe0b409937256746",
+    "url": "/portflio-2.0/static/media/svg-bg.e187a41a.svg"
+  }
+]);
