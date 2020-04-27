f845ed84e31b182a09be3042b476b9a72e14b11d
Joaquin Fernandez <copako12@gmail.com>
2020-04-27 17:48:52 -0300

    NOW IS FINISHED WAITING 4 MORE PROJECTS

--- src/components/Content/Projects.js
+++ src/components/Content/Projects.js
@@ -1,36 +1,38 @@
-import React from 'react';
-import '../styles/Projects.scss';
-import ProjectItem from './ProjectItem';
+import React from "react";
+import "../styles/Projects.scss";
+import ProjectItem from "./ProjectItem";
 //Json
-import projects from '../data/projects.json';
+import projects from "../data/projects.json";
 
 class Projects extends React.Component {
-	render() {
-		let key = 0;
-		let projectsList = projects.map((project) => {
-			key++;
-			return (
-				<ProjectItem
-					description={project.description}
-					key={key}
-					github={project.github}
-					website={project.website}
-					img={project.img}
-					tech={project.tech}
-				/>
-			);
-		});
+  render() {
+    let key = 0;
+    let projectsList = projects.map((project) => {
+      key++;
+      return (
+        <ProjectItem
+          description={project.description}
+          key={key}
+          github={project.github}
+          website={project.website}
+          img={project.img}
+          tech={project.tech}
+        />
+      );
+    });
 
-		return (
-			<div className="projects">
-				{projectsList}
-
-				<h1 style={{ paddingTop: '3em', paddingBottom: '3em' }}>
-					And more to come...
-				</h1>
-			</div>
-		);
-	}
+    return (
+      <div className="projects">
+        {projectsList}
+        <div style={{ padding: "3em 1em" }}>
+          <h1>And more to come...</h1>
+          <p>
+            (if you really want to see more, in my github i have more projects)
+          </p>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Projects;
