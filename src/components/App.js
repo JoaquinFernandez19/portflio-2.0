8b989d0eede2c0e15a25eb2f37f778d65c17bbc3
Joaquin Fernandez <copako12@gmail.com>
2020-04-28 04:35:23 -0300

    remove memory leak problems

--- src/components/App.js
+++ src/components/App.js
@@ -1,62 +1,74 @@
-import React from 'react';
+import React from "react";
 //Style
-import './styles/App.scss';
+import "./styles/App.scss";
 
 //Components
 
-import Menu from './Menu';
-import Title from './Title';
-import Content from './Content';
+import Menu from "./Menu";
+import Title from "./Title";
+import Content from "./Content";
 //
 class App extends React.Component {
-	state = {
-		currentTab: 'Projects',
-		mobileState: null,
-	};
+  state = {
+    currentTab: "Projects",
+    mobileState: null,
+  };
 
-	handleSelect = (selected) => {
-		this.setState({ currentTab: selected });
-	};
+  handleSelect = (selected) => {
+    this.setState({ currentTab: selected });
+  };
 
-	componentDidMount = (prevState) => {
-		const menu = document.querySelector('.menu');
-		if (prevState !== this.state) {
-			window.addEventListener('resize', () => {
-				if (window.innerWidth <= 798) {
-					menu.style.display = 'none';
-					this.setState({ mobileState: true });
-				} else {
-					this.setState({ mobileState: false });
-				}
-			});
-		}
-	};
+  componentDidMount = (prevState) => {
+    const menu = document.querySelector(".menu");
+    if (prevState !== this.state) {
+      window.addEventListener("resize", () => {
+        if (window.innerWidth <= 798) {
+          menu.style.display = "none";
+          this.setState({ mobileState: true });
+        } else {
+          this.setState({ mobileState: false });
+        }
+      });
+    }
+  };
 
-	render() {
-		return (
-			<div className="main-container">
-				<Title showMenu={this.showMenu} />
-				<Menu
-					handleSelect={this.handleSelect}
-					mobileState={this.state.mobileState}
-					active={this.state.currentTab}
-				/>
-				<div className="current-tab-title">
-					<h1>
-						{this.state.currentTab}
-						<span
-							className="animated flash infinite"
-							style={{
-								zIndex: '1',
-							}}>
-							|
-						</span>
-					</h1>
-				</div>
-				<Content current={this.state.currentTab} />
-			</div>
-		);
-	}
+  componentWillUnmount() {
+    const menu = document.querySelector(".menu");
+    window.removeEventListener("resize", () => {
+      if (window.innerWidth <= 798) {
+        menu.style.display = "none";
+        this.setState({ mobileState: true });
+      } else {
+        this.setState({ mobileState: false });
+      }
+    });
+  }
+  render() {
+    return (
+      <div className="main-container">
+        <Title showMenu={this.showMenu} />
+        <Menu
+          handleSelect={this.handleSelect}
+          mobileState={this.state.mobileState}
+          active={this.state.currentTab}
+        />
+        <div className="current-tab-title">
+          <h1>
+            {this.state.currentTab}
+            <span
+              className="animated flash infinite"
+              style={{
+                zIndex: "1",
+              }}
+            >
+              |
+            </span>
+          </h1>
+        </div>
+        <Content current={this.state.currentTab} />
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,81 +1,89 @@
-import React from 'react';
-import './styles/Menu.scss';
+import React from "react";
+import "./styles/Menu.scss";
 //Icons
 import {
-	RiProjectorLine,
-	RiTerminalBoxLine,
-	RiBook2Line,
-} from 'react-icons/ri';
-import { AiOutlineStar } from 'react-icons/ai';
+  RiProjectorLine,
+  RiTerminalBoxLine,
+  RiBook2Line,
+} from "react-icons/ri";
+import { AiOutlineStar } from "react-icons/ai";
 
 //
 class Menu extends React.Component {
-	state = { mobile: null };
+  state = { mobile: null };
 
-	handleClick = (e) => {
-		const menu = document.querySelector('.menu');
-		if (this.state.mobile || this.props.mobileState) {
-			menu.style.display = 'none';
-		}
-		this.props.handleSelect(e.target.getAttribute('value'));
-		
+  handleClick = (e) => {
+    const menu = document.querySelector(".menu");
+    if (this.state.mobile || this.props.mobileState) {
+      menu.style.display = "none";
+    }
+    this.props.handleSelect(e.target.getAttribute("value"));
 
-		//active class
-		let current = e.target;
-		let arrayParent = Array.from(e.target.parentNode.children);
-		arrayParent.forEach((item)=>{
-			if(item === current){
-				item.classList.add('active');
-			}
-			if(item !== current) {
-				item.classList.remove('active');
-			}
-		})
-	};
+    //active class
+    let current = e.target;
+    let arrayParent = Array.from(e.target.parentNode.children);
+    arrayParent.forEach((item) => {
+      if (item === current) {
+        item.classList.add("active");
+      }
+      if (item !== current) {
+        item.classList.remove("active");
+      }
+    });
+  };
 
-	componentDidMount() {
-		const menu = document.querySelector('.menu');
-		window.addEventListener('resize', () => {
-			if (window.innerWidth >= 798) {
-				menu.style.display = 'block';
-				this.setState({ mobile: false });
-			}
-		});
-		if (window.innerWidth <= 798) {
-			this.setState({ mobile: true });
-		}
-	}
+  componentDidMount() {
+    const menu = document.querySelector(".menu");
+    window.addEventListener("resize", () => {
+      if (window.innerWidth >= 798) {
+        menu.style.display = "block";
+        this.setState({ mobile: false });
+      }
+    });
+    if (window.innerWidth <= 798) {
+      this.setState({ mobile: true });
+    }
+  }
+  componentWillUnmount() {
+    const menu = document.querySelector(".menu");
+    window.removeEventListener("resize", () => {
+      if (window.innerWidth <= 798) {
+        menu.style.display = "none";
+        this.setState({ mobileState: true });
+      } else {
+        this.setState({ mobileState: false });
+      }
+    });
+  }
 
-	render() {
-
-
-
-		return (
-			<div className="menu">
-				<ul className="animated slideInDown">
-					<div
-						className='menu-item active'
-						value="Projects"
-						onClick={this.handleClick}>
-						<RiProjectorLine className="menu-icon" />
-						<li>Projects</li>
-					</div>
-					<div className="menu-item" value="Skills" onClick={this.handleClick}>
-						<RiTerminalBoxLine className="menu-icon" />
-						<li>Skills</li>
-					</div>
-					<div className="menu-item" value="About" onClick={this.handleClick}>
-						<RiBook2Line className="menu-icon" />
-						<li>About</li>
-					</div>
-					<div className="menu-item" value="Extra" onClick={this.handleClick}>
-						<AiOutlineStar className="menu-icon" />
-						<li>Extra</li>
-					</div>
-				</ul>
-			</div>
-		);
-	}
+  render() {
+    return (
+      <div className="menu">
+        <ul className="animated slideInDown">
+          <div
+            className="menu-item active"
+            value="Projects"
+            onClick={this.handleClick}
+          >
+            <RiProjectorLine className="menu-icon" />
+            <li>Projects</li>
+          </div>
+          <div className="menu-item" value="Skills" onClick={this.handleClick}>
+            <RiTerminalBoxLine className="menu-icon" />
+            <li>Skills</li>
+          </div>
+          <div className="menu-item" value="About" onClick={this.handleClick}>
+            <RiBook2Line className="menu-icon" />
+            <li>About</li>
+          </div>
+          <div className="menu-item" value="Extra" onClick={this.handleClick}>
+            <AiOutlineStar className="menu-icon" />
+            <li>Extra</li>
+          </div>
+        </ul>
+      </div>
+    );
+  }
 }
 
 export default Menu;
